50ee6648e0f5b3487138c1047086c6491f878a53
CristianoCap <cristiano.antonio.capanna@gmail.com>
2017-05-01 13:28:39 +0200

    Add files via upload

---
+++ app/pizzaView/pizzaView.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp.pizzaView', ['ngRoute','myApp.pizza'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/pizzaView', {
+    templateUrl: 'pizzaView/pizzaView.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('ViewCtrl', ['$scope', 'Pizza',
+function($scope,Pizza) {
+  Pizza.getData().then(function(data) {
+      $scope.dati = {};
+      $scope.dati.pizzas = data;
+  });
+
+}]);
